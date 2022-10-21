92722e144743f49c89d0cb21c7a627b0c3af6a17
Matthieu <matthieu@pixel-developpement.com>
2022-10-21 15:06:17 +0200

    + add prestashop/php-dev-tools

--- pixel_cloudflare_turnstile.php
+++ pixel_cloudflare_turnstile.php
@@ -60,9 +60,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
         $this->templateFile = 'module:' . $this->name . '/pixel_cloudflare_turnstile.tpl';
     }
 
-    /***************************/
     /** MODULE INITIALIZATION **/
-    /***************************/
 
     /**
      * Install the module
@@ -97,9 +95,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
         return true;
     }
 
-    /***********/
     /** HOOKS **/
-    /***********/
 
     /**
      * Adds CSS and JS
@@ -112,17 +108,17 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
             'cloudflare-turnstile',
             'modules/' . $this->name . '/views/css/turnstile.css',
             [
-                'position'   => 'head',
-                'priority'   => 100,
+                'position' => 'head',
+                'priority' => 100,
             ]
         );
         $this->context->controller->registerJavascript(
             'cloudflare-turnstile',
             'https://challenges.cloudflare.com/turnstile/v0/api.js',
             [
-                'server'     => 'remote',
-                'position'   => 'head',
-                'priority'   => 100,
+                'server' => 'remote',
+                'position' => 'head',
+                'priority' => 100,
                 'attributes' => 'async',
             ]
         );
@@ -141,6 +137,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
         if (!$this->isAvailable(self::FORM_REGISTER)) {
             return '';
         }
+
         return $this->renderWidget('displayCustomerAccountForm', []);
     }
 
@@ -150,6 +147,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
      * @param array $params
      *
      * @return void
+     *
      * @throws Exception
      */
     public function hookActionFrontControllerInitBefore(array $params): void
@@ -164,6 +162,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
                 [],
                 'Modules.Pixelcloudflareturnstile.Shop'
             );
+
             return;
         }
         if (!$this->getSitekey()) {
@@ -172,6 +171,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
                 [],
                 'Modules.Pixelcloudflareturnstile.Shop'
             );
+
             return;
         }
 
@@ -195,7 +195,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
      * Check if turnstile is available for current action
      *
      * @param string $controllerClass
-     * @param bool   $validate
+     * @param bool $validate
      *
      * @return bool
      */
@@ -212,6 +212,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
             if ($validate && !Tools::isSubmit('submitMessage')) {
                 return false;
             }
+
             return true;
         }
 
@@ -224,6 +225,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
             if ($validate && !Tools::isSubmit('submitCreate')) {
                 return false;
             }
+
             return true;
         }
 
@@ -236,6 +238,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
             if ($validate && !Tools::isSubmit('submitLogin')) {
                 return false;
             }
+
             return true;
         }
 
@@ -244,6 +247,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
             if ($validate && empty($_POST)) {
                 return false;
             }
+
             return true;
         }
 
@@ -266,12 +270,13 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
      * Validate turnstile
      *
      * @return void
+     *
      * @throws Exception
      */
     public static function turnstileValidation(): void
     {
         $referer = $_SERVER['HTTP_REFERER'] ?? 'index';
-        $cookie  = Context::getContext()->cookie;
+        $cookie = Context::getContext()->cookie;
 
         $response = Tools::getValue('cf-turnstile-response');
         if (!$response) {
@@ -287,7 +292,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
         }
 
         $data = [
-            'secret'   => Configuration::get(self::CONFIG_CLOUDFLARE_TURNSTILE_SECRET_KEY),
+            'secret' => Configuration::get(self::CONFIG_CLOUDFLARE_TURNSTILE_SECRET_KEY),
             'response' => $response,
         ];
 
@@ -328,22 +333,20 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
     protected static function getErrorMessage(string $code): string
     {
         $messages = [
-            'missing-input-secret'   => 'the secret parameter was not passed.',
-            'invalid-input-secret'   => 'the secret parameter was invalid or did not exist.',
+            'missing-input-secret' => 'the secret parameter was not passed.',
+            'invalid-input-secret' => 'the secret parameter was invalid or did not exist.',
             'missing-input-response' => 'the response parameter was not passed.',
             'invalid-input-response' => 'the response parameter is invalid or has expired.',
-            'bad-request'            => 'the request was rejected because it was malformed.',
-            'timeout-or-duplicate'   => 'the response parameter has already been validated before.',
-            'internal-error'         => 'an internal error happened while validating the response. The request can be retried.',
-            'unavailable'            => 'unable to contact Cloudflare to validate the form',
+            'bad-request' => 'the request was rejected because it was malformed.',
+            'timeout-or-duplicate' => 'the response parameter has already been validated before.',
+            'internal-error' => 'an internal error happened while validating the response. The request can be retried.',
+            'unavailable' => 'unable to contact Cloudflare to validate the form',
         ];
 
         return $messages[$code] ?? 'unknown error';
     }
 
-    /*********************/
     /** FRONTEND WIDGET **/
-    /*********************/
 
     /**
      * Render the turnstile widget
@@ -380,8 +383,8 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
     {
         return [
             'sitekey' => $this->getSitekey(),
-            'theme'   => $configuration['theme'] ?? $this->getTheme(),
-            'action'  => $configuration['action'] ?? $this->getFormName(),
+            'theme' => $configuration['theme'] ?? $this->getTheme(),
+            'action' => $configuration['action'] ?? $this->getFormName(),
         ];
     }
 
@@ -410,9 +413,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
         return $action;
     }
 
-    /*************************/
     /** ADMIN CONFIGURATION **/
-    /*************************/
 
     /**
      * Retrieve config fields
@@ -423,67 +424,67 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
     {
         return [
             self::CONFIG_CLOUDFLARE_TURNSTILE_SITEKEY => [
-                'type'     => 'text',
-                'label'    => $this->trans('Sitekey', [], 'Modules.Pixelcloudflareturnstile.Admin'),
-                'name'     => self::CONFIG_CLOUDFLARE_TURNSTILE_SITEKEY,
+                'type' => 'text',
+                'label' => $this->trans('Sitekey', [], 'Modules.Pixelcloudflareturnstile.Admin'),
+                'name' => self::CONFIG_CLOUDFLARE_TURNSTILE_SITEKEY,
                 'required' => true,
             ],
             self::CONFIG_CLOUDFLARE_TURNSTILE_SECRET_KEY => [
-                'type'     => 'text',
-                'label'    => $this->trans('Secret key', [], 'Modules.Pixelcloudflareturnstile.Admin'),
-                'name'     => self::CONFIG_CLOUDFLARE_TURNSTILE_SECRET_KEY,
+                'type' => 'text',
+                'label' => $this->trans('Secret key', [], 'Modules.Pixelcloudflareturnstile.Admin'),
+                'name' => self::CONFIG_CLOUDFLARE_TURNSTILE_SECRET_KEY,
                 'required' => true,
             ],
             self::CONFIG_CLOUDFLARE_TURNSTILE_THEME => [
-                'type'     => 'select',
-                'label'    => $this->trans('Theme', [], 'Modules.Pixelcloudflareturnstile.Admin'),
-                'name'     => self::CONFIG_CLOUDFLARE_TURNSTILE_THEME,
+                'type' => 'select',
+                'label' => $this->trans('Theme', [], 'Modules.Pixelcloudflareturnstile.Admin'),
+                'name' => self::CONFIG_CLOUDFLARE_TURNSTILE_THEME,
                 'required' => true,
                 'options' => [
                     'query' => [
                         [
                             'value' => 'auto',
-                            'name'  => 'Auto',
+                            'name' => 'Auto',
                         ],
                         [
                             'value' => 'light',
-                            'name'  => 'Light',
+                            'name' => 'Light',
                         ],
                         [
                             'value' => 'dark',
-                            'name'  => 'Dark',
+                            'name' => 'Dark',
                         ],
                     ],
-                    'id'   => 'value',
+                    'id' => 'value',
                     'name' => 'name',
                 ],
             ],
             self::CONFIG_CLOUDFLARE_TURNSTILE_FORMS => [
-                'type'     => 'select',
+                'type' => 'select',
                 'multiple' => true,
-                'label'    => $this->trans('Forms to validate', [], 'Modules.Pixelcloudflareturnstile.Admin'),
-                'name'     => self::CONFIG_CLOUDFLARE_TURNSTILE_FORMS . '[]',
+                'label' => $this->trans('Forms to validate', [], 'Modules.Pixelcloudflareturnstile.Admin'),
+                'name' => self::CONFIG_CLOUDFLARE_TURNSTILE_FORMS . '[]',
                 'required' => false,
                 'options' => [
                     'query' => [
                         [
                             'value' => self::FORM_CONTACT,
-                            'name'  => $this->trans('Contact', [], 'Modules.Pixelcloudflareturnstile.Admin'),
+                            'name' => $this->trans('Contact', [], 'Modules.Pixelcloudflareturnstile.Admin'),
                         ],
                         [
                             'value' => self::FORM_LOGIN,
-                            'name'  => $this->trans('Login', [], 'Modules.Pixelcloudflareturnstile.Admin'),
+                            'name' => $this->trans('Login', [], 'Modules.Pixelcloudflareturnstile.Admin'),
                         ],
                         [
                             'value' => self::FORM_REGISTER,
-                            'name'  => $this->trans('Register', [], 'Modules.Pixelcloudflareturnstile.Admin'),
+                            'name' => $this->trans('Register', [], 'Modules.Pixelcloudflareturnstile.Admin'),
                         ],
                         [
                             'value' => self::FORM_PASSWORD,
-                            'name'  => $this->trans('Reset Password', [], 'Modules.Pixelcloudflareturnstile.Admin'),
+                            'name' => $this->trans('Reset Password', [], 'Modules.Pixelcloudflareturnstile.Admin'),
                         ],
                     ],
-                    'id'   => 'value',
+                    'id' => 'value',
                     'name' => 'name',
                 ],
                 'desc' => $this->trans(
@@ -499,6 +500,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
      * This method handles the module's configuration page
      *
      * @return string
+     *
      * @throws Exception
      */
     public function getContent(): string
@@ -547,6 +549,7 @@ class Pixel_cloudflare_turnstile extends Module implements WidgetInterface
      * Retrieve current theme name
      *
      * @return string
+     *
      * @throws Exception
      */
     public function getCurrentThemeName(): string
